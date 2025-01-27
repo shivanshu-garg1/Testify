d343f4553bab2de51cb8847836982f92470c08a9
Shivanshu garg <gargshivanshu000@gmail.com>
2025-01-27 12:04:32 +0530

    Second Commit

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ export default function Navbar() {
       <Link to="/" className='hover:text-lg'>Home</Link>
       <Link to="/about">About</Link>
       <Link to="/about">Contact us</Link>
+      <Link to="/about">Contact us</Link>
     </div>
   </nav>
   );
