65a4f8fce0dc17776e248d3687c65de6d95e89d3
Shivanshu garg <gargshivanshu000@gmail.com>
2025-03-02 23:00:16 +0530

    Updated

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -11,7 +11,7 @@
       e.preventDefault();
       try{
         
-        const res = await fetch("http://localhost:3000/api/auth/login",{
+        const res = await fetch("https://testify-server-mocha.vercel.app/api/auth/login",{
           method:"POST",
           headers:{"Content-Type":"application/json"},
           body:JSON.stringify({email,password})
--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -17,7 +17,7 @@ const Signup = () => {
   const handelSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:3000/api/auth/signup", {
+      const res = await fetch("https://testify-server-mocha.vercel.app/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
